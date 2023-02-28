b01ea63acc75f397247cf413b2573fd5836a61f1
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-02-28 11:12:48 +0100

    add @types/node

--- dist/index.js
+++ dist/index.js
@@ -9723,7 +9723,7 @@ const { PARSER_CONTENT } = __nccwpck_require__(1304);
 async function getAllNodesDisplayNames() {
   try {
     await exec(
-      `npm i typescript@4.8.4 fast-glob; npm i -g ts-node@10.9.1 ; touch parser.ts; echo "${PARSER_CONTENT}" > parser.ts`,
+      `npm i typescript@4.9 @types/node@16.18 fast-glob; npm i -g ts-node@10.9 ; touch parser.ts; echo "${PARSER_CONTENT}" > parser.ts`,
     );
     const result = await exec("ts-node parser.ts");
 
