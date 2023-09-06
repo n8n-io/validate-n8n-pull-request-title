114293b73e0c5082a96b99d29c5f164794bd8e90
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-09-06 10:55:41 +0200

    maintain the parser as a file instead of as a string

--- src/getAllNodesDisplayNames.js
+++ src/getAllNodesDisplayNames.js
@@ -1,7 +1,6 @@
 const { exec: callbackExec } = require("child_process");
 const { promisify } = require("util");
 const exec = promisify(callbackExec);
-const { PARSER_CONTENT } = require("./parserContent");
 
 /**
  * @returns { Promise<string[]> } e.g. ["Action Network", "Active Campaign", etc.]
@@ -9,12 +8,10 @@ const { PARSER_CONTENT } = require("./parserContent");
 async function getAllNodesDisplayNames() {
   try {
     await exec(
-      `npm i typescript@5.2 @types/node@18.16 fast-glob; npm i -g ts-node@10.9 ; touch parser.ts; echo "${PARSER_CONTENT}" > parser.ts`,
+      "npm i typescript@5.2 @types/node@18.16 fast-glob; npm i -g ts-node@10.9",
     );
+    await exec(`cp "${__dirname}/parser.ts" parser.ts`);
     const result = await exec("ts-node parser.ts");
-
-    console.log(result);
-
     return JSON.parse(result.stdout);
   } catch (error) {
     console.error("Failed to generate list of node display names");
