e1f2af51105b1f5ef0e922aabc4e52091daf2aa0
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-01-26 10:45:02 +0100

    Upgrade dependencies, and migrate to Node.js 20

--- src/getAllNodesDisplayNames.js
+++ src/getAllNodesDisplayNames.js
@@ -8,7 +8,7 @@ const exec = promisify(callbackExec);
 async function getAllNodesDisplayNames() {
   try {
     await exec(
-      "npm i typescript@5.2 @types/node@18.16 fast-glob; npm i -g ts-node@10.9",
+      "npm i typescript@5.3 @types/node@18.16 fast-glob; npm i -g ts-node@10.9",
     );
     await exec(`cp "${__dirname}/../src/parser.ts" parser.ts`);
     const result = await exec("ts-node parser.ts");
