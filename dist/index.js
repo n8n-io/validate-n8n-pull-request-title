71cb76ec15fe55002d4eddd5cb15d47065a501a8
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2026-08-17 14:38:33 +0300

    feat: Add engine scope
    Adds the "engine" scope for the new workflow execution engine 2.0.

--- dist/index.js
+++ dist/index.js
@@ -29071,7 +29071,7 @@ const TYPES = [
   "chore",
 ];
 
-const SCOPES = ["API", "core", "editor", "benchmark", "ai-builder"];
+const SCOPES = ["API", "core", "editor", "benchmark", "ai-builder", "engine"];
 
 const displayTypes = TYPES.map((type) => `\`${type}\``).join(", ");
 const displayScopes = SCOPES.map((type) => `\`${type}\``).join(", ");
